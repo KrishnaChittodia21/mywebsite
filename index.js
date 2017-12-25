bc07d65b35af04aba51f25a7f0d31cee5ccbda1a
krishnachittodia21 <krishnachittodia.10@gmail.com>
2017-12-26 05:17:23 +0530

    26-12-2017

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post('/send',(req,res) => {
         secure: false, // true for 465, false for other ports
         auth: {
             user: 'kannudoe@gmail.com', // generated ethereal user
-            pass: 'ihavenofear'  // generated ethereal password
+            pass: ''  // generated ethereal password
         },
         tls:{
           rejectUnauthorized:false
